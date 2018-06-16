e9195e5e6143eb3e8bdb2864f52e37e377339f07
James Feore <jjfeore@gmail.com>
2018-06-16 02:14:41 -0700

    Building initial event framework

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 const NodeMediaServer = require('node-media-server'),
-    tmi = require("tmi.js");
+    tmi = require("tmi.js"),
+    OBSWebSocket = require('obs-websocket-js');
 
 // Configure Node media server
 const nmsConfig = {
@@ -21,6 +22,25 @@ const nmsConfig = {
 let nms = new NodeMediaServer(nmsConfig);
 nms.run();
 
+// Configure OBS websocket
+const obs = new OBSWebSocket();
+obs.connect({
+    address: 'localhost:4444',
+    password: process.env.OBSWSPASS
+}).then(() => {
+    console.log(`Connected to OBS via Websocket`);
+    return obs.getSceneList();
+}).then((sceneList) => {
+    let sceneListNames = 'Available Scenes: ';
+    for (let scene of sceneList) {
+        sceneListNames += scene.name + ', ';
+    }
+    sceneListNames = sceneListNames.slice(0, -2);
+    console.log(sceneListNames);
+}).catch(err => {
+    console.log(err);
+});
+
 // Configure Twitch chat client
 const twitchOptions = {
     options: {
@@ -36,5 +56,39 @@ const twitchOptions = {
     channels: ["#banzaibaby"]
 };
 
-let twitchClient = new tmi.client(twitchOptions);
+const twitchClient = new tmi.client(twitchOptions);
 twitchClient.connect();
+
+// On RTMP connection to server
+nms.on('postConnect', (id, args) => {
+    console.log('[NodeEvent on postConnect]', `id=${id} args=${JSON.stringify(args)}`);
+});
+
+// On RTMP connection to server
+nms.on('doneConnect', (id, args) => {
+    console.log('[NodeEvent on doneConnect]', `id=${id} args=${JSON.stringify(args)}`);
+});
+
+// On Twitch message
+client.on("chat", function (channel, user, message, self) {
+    if (self || user.mod) {
+        console.log(user.display-name + ': ' + message);
+        if (message.startsWith('!scene ')) {
+            let toScene = message.slice(7);
+        } else if (message.startsWith('!disconnect')) {
+            let streamStatus = obs.getStreamingStatus();
+            streamStatus.then(function (err, data) {
+                if (err) {
+                    console.error('OBS Socket Error:', err);
+                } else if (data.streaming) {
+                    obs.stopStreaming();
+                }
+            });
+        }
+    }
+});
+
+// Catch all uncaught exceptions
+obs.on('error', err => {
+	console.error('OBS Socket Error:', err);
+});
